8b0804c502b4e9537804ad96a492c13e85206807
ken <limstar9147@gmail.com>
2025-09-06 03:08:54 +0900

    Update Day01.lua

--- Day01.lua
+++ Day01.lua
@@ -1,4 +1,27 @@
+--[]
 print("Hello!")
 print("How are you doing?")
 print("I'm doing very well!")
 
+print(true)
+print(false)
+
+print(100)
+print(-100)
+print(100.50)
+
+print("This is a string.")
+print('This is a string.')
+
+--[]
+local myVariableIsFifty = 50
+print(myVariableIsFifty)
+print(myVariableIsFifty)
+print(myVariableIsFifty)
+
+--[]
+local baseplate = game.Workspace.Baseplate
+
+baseplate.Transparency = 0.25
+baseplate.Material = "Brick"
+baseplate.CastShadow = false
